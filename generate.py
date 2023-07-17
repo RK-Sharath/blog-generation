7c6ce1d7dc1b3b0d76f4016713694ba15cba43cf
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-17 14:55:48 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ from genai.schemas import ModelType, GenerateParams
 from genai.model import Credentials
 
 st.title("Blog Generator App")
-st.subheader("This app was developed by Sharath Kumar RK, Ecosystem Engineering Watsonx team")
+st.caption("This app was developed by Sharath Kumar RK, Ecosystem Engineering Watsonx team")
 
 genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
 genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
