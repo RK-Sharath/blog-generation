e81cfd8745a99a6362258fd36f3ade1e0e033ccf
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-17 15:12:45 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -10,13 +10,19 @@ st.caption("This app was developed by Sharath Kumar RK, Ecosystem Engineering Wa
 
 genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
 genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
+max_tokens = st.sidebar.text_input("Max new tokens", type="default")
+min_tokens = st.sidebar.text_input("Min new tokens", type="default")
+decoding_method = st.sidebar.text_input("Decoding method", type="default")
+repetition_penalty = st.sidebar.text_input("Repetition penalty", type="default")
+temperature = st.sidebar.text_input("Temperature", type="default")
+
 
 
 def blog_outline(topic):
     # Create a creds object
     creds = Credentials(api_key=genai_api_key, api_endpoint=genai_api_url)
     # Define parameters
-    params = GenerateParams(decoding_method="sample", temperature=0.7, max_new_tokens=400, min_new_tokens=150, repetition_penalty=2)
+    params = GenerateParams(decoding_method=decoding_method, temperature=temperature, max_new_tokens=max_tokens, min_new_tokens=min_tokens, repetition_penalty=repetition_penalty)
     # Instantiate LLM model
     llm=LangChainInterface(model=ModelType.FLAN_T5_11B, params=params, credentials=creds)
     # Prompt
