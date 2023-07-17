708828a3d16f8b040f72bc9b3799f7bd0cac1173
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-17 17:11:33 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -38,6 +38,8 @@ def blog_outline(topic):
 with st.form("myform"):
     topic_text = st.text_input("Enter prompt:", "")
     submitted = st.form_submit_button("Submit")
+    if submitted and genai_api_key.startswith('pak-'):
+        with st.spinner('Working on it...'):
     if not genai_api_key:
         st.info("Please add your GenAI API KEY & GenAI API URL to continue.")
     elif submitted:
